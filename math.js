86a09e6cb6780796d712c42f30c60c7a07762d0c
Rusillionchik <113810196+Rusillionchik@users.noreply.github.com>
2023-11-13 13:47:04 +0300

    Add files via upload

---
+++ math.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => a + b
+
+
+const substract = (a, b) => a - b
+
+
+const multiply = (a, b) => a * b
+
+
+const division = (a, b) => {
+if (b === 0) {
+return 'Нельзя делить число на ноль'
+}
+return a / b
+}
